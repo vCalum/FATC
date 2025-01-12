b6f5c28eebb3dd15b99812f86eccd6ac5381130d
vCalum <cvermeulen2002@gmail.com>
2025-01-12 18:51:07 +0000

    adding declarations and basic funcs for basic FAT

--- filesys.c
+++ filesys.c
@@ -1,1 +1,33 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include "filesys.h"
+
+diskblock_t virtualDisk[MAXBLOCKS];
+fatentry_t FAT[MAXBLOCKS];
+fatentry_t rootDirIndex = FATBLOCKSNEEDED + 1;
+dirblock_t * rootDirBlock_ptr = NULL;
+fatentry_t currentDirIndex = 0;
+dirblock_t * currentDirBlock_ptr = NULL;
+const char parse[2] = "/";
+
+void writeDisk() {
+
+}
+
+void readDisk() {
+
+}
+
+void writeBlock() {
+
+}
+
+void format() {
+
+}
+
+void copyFAT() {
+
+}
